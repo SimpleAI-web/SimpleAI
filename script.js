1ecb94fa2c17abb862e468eed1537a7fe93ebba9
SimpleAI-web <simpleai19@outlook.com>
2026-08-11 16:55:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -466,7 +466,7 @@ async function sendMessage() {
         });
 
         const apiKeyDecoded = atob(encodedKey);
-const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKeyDecoded}`, {
+const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKeyDecoded}`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ contents: conversation })
